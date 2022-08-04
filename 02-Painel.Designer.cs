d63db06ec8386011315f8f9734ac443b4e65dfed
Patrick M <patrick.msilva5@senacsp.edu.br>
2022-08-04 11:17:15 -0300

    create 02-Painel

---
+++ 02-Painel.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace sys_bdourados
+{
+    partial class frmPainel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.close = new System.Windows.Forms.PictureBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblFundo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.close)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // close
+            // 
+            this.close.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.close.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.close.Image = global::sys_bdourados.Properties.Resources.close;
+            this.close.Location = new System.Drawing.Point(973, 3);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(24, 24);
+            this.close.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.close.TabIndex = 7;
+            this.close.TabStop = false;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.lblTitulo.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(196)))), ((int)(((byte)(196)))));
+            this.lblTitulo.Location = new System.Drawing.Point(5, 5);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(138, 28);
+            this.lblTitulo.TabIndex = 6;
+            this.lblTitulo.Text = "Painel de ações";
+            // 
+            // lblFundo
+            // 
+            this.lblFundo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.lblFundo.Location = new System.Drawing.Point(0, 30);
+            this.lblFundo.Name = "lblFundo";
+            this.lblFundo.Size = new System.Drawing.Size(1000, 570);
+            this.lblFundo.TabIndex = 8;
+            // 
+            // frmPainel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.ClientSize = new System.Drawing.Size(1000, 600);
+            this.Controls.Add(this.lblFundo);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.lblTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmPainel";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DouradosSys | Painel";
+            ((System.ComponentModel.ISupportInitialize)(this.close)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox close;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblFundo;
+    }
+}
